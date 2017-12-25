30868c43d41206fbba223ceea30edc447a0215ce
leonardo viveiros <leonardoviveirossantos@gmail.com>
2017-12-25 12:17:15 -0200

    test get /hotels/all

--- src/main/java/com/example/demo/controller/HotelController.java
+++ src/main/java/com/example/demo/controller/HotelController.java
@@ -1,27 +1,28 @@
-package com.example.controller;
+package com.example.demo.controller;
 
-import com.example.dao.HotelRepository;
+import com.example.demo.dao.HotelRepository;
 import com.example.model.Hotel;
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hotels")
 public class HotelController {
 
+    @Autowired
     private final HotelRepository repository;
 
     public HotelController(HotelRepository repository) {
         this.repository = repository;
     }
 
-    @GetMapping("/all")
+    @RequestMapping("/all")
     public List<Hotel> getAll() {
         List<Hotel> hotels = repository.findAll();
-
+        hotels.add(new Hotel("hardcoded test", 0, null, null));
         return hotels;
     }
+
 }
--- src/main/java/com/example/demo/dao/HotelRepository.java
+++ src/main/java/com/example/demo/dao/HotelRepository.java
@@ -1,10 +1,13 @@
-package com.example.dao;
+package com.example.demo.dao;
 
 import com.example.model.Hotel;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HotelRepository extends MongoRepository<Hotel, String> {
 
+    @Override
+    public List<Hotel> findAll();
 }
