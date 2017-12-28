b935f358a6a6bf9d97108543f710217470416d2c
leonardo viveiros <leonardoviveirossantos@gmail.com>
2017-12-28 21:03:11 -0200

    Adding getOne method on url hotels/id

--- src/main/java/com/example/demo/controller/HotelController.java
+++ src/main/java/com/example/demo/controller/HotelController.java
@@ -4,9 +4,12 @@ import com.example.demo.dao.HotelRepository;
 import com.example.demo.model.Hotel;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,5 +33,13 @@ public class HotelController {
     public void newHotel(@RequestBody Hotel hotel) {
         hotel.setId(null); //a new hotel must have null ID so MongoDB creates one
         repository.save(hotel);
+    }
+
+    @RequestMapping(value = "/{hotelId}", method = RequestMethod.GET, produces = "application/json")
+    public Hotel getOne(@PathVariable String hotelId) {
+        System.out.println("chegou no metodo");
+        System.out.println("id=" + hotelId);
+        Hotel hotel = repository.findOne(hotelId);
+        return hotel;
     }
 }
